4a8ff75e93a884d5fb522874841092dd7c4ec923
Dima <skydmytro@gmail.com>
2024-12-23 18:45:34 +0100

    feat: implement user registration page and API request functionality

---
+++ src/config/routes.ts
@@ -0,0 +1,11 @@
+export const publicRoutes = ['/', '/login', '/register', '/about'];
+
+export const authRoutes = ['/login', '/register'];
+
+export const protectedRoutes = [
+  '/dashboard',
+  '/profile',
+  '/wishlists/:id',
+  '/users/:userId',
+  '/users/:userId/wishlists',
+];
--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,3 +1,3 @@
-export const API_URL = process.env.API_URL;
+export const API_URL = process.env.NEXT_PUBLIC_API_URL;
 export const wishlistUrl = '/api/v1/wish-list';
 export const userUrl = '/api/users';

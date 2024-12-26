e2d81f3dc7ed3f190e4b360ecacb075c38728251
Dima <skydmytro@gmail.com>
2024-12-26 17:15:12 +0100

    feat: enhance middleware for JWT handling and user redirection

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,19 +2,26 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 import { isPublicRoute } from './routes';
+import decodeJWT from './utils/decodeJwt';
 
 export function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname;
+  const token = request.cookies.get('jwt')?.value;
 
   if (path === '/') {
     return NextResponse.redirect(new URL('/home', request.url));
   }
 
   if (!isPublicRoute(path)) {
-    const token = request.cookies.get('jwt')?.value;
     if (!token) {
       return NextResponse.redirect(new URL('/login', request.url));
     }
+  }
+
+  if (token && (path === '/login' || path === '/register')) {
+    const decoded = decodeJWT(token || ' ');
+    console.log('decoded', decoded);
+    return NextResponse.redirect(new URL(`/users/${decoded._id}`, request.url));
   }
 
   return NextResponse.next();

20b7779f0bcbe3d8b8d294c70a45cacdd864d34c
Dima <skydmytro@gmail.com>
2025-01-13 16:13:30 +0100

    refactor: delete comments

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,3 @@
-// import { NextResponse } from 'next/server';
-// import type { NextRequest } from 'next/server';
-// import { isPublicRoute } from './routes';
-// import decodeJWT from './utils/decodeJwt';
-// export { auth as middleware } from '@/auth/authSetup';
 import { auth } from '@/auth/authSetup';
 
 export default auth((req) => {
@@ -23,32 +18,6 @@ export default auth((req) => {
     return Response.redirect(newUrl);
   }
 });
-// export function middleware(request: NextRequest) {
-//   const path = request.nextUrl.pathname;
-//   const token = request.cookies.get('jwt')?.value;
-
-//   if (path === '/') {
-//     return NextResponse.redirect(new URL('/home', request.url));
-//   }
-
-//   if (!isPublicRoute(path)) {
-//     if (!token) {
-//       return NextResponse.redirect(new URL('/login', request.url));
-//     }
-//   }
-
-//   if (token && (path === '/login' || path === '/register')) {
-//     const decoded = decodeJWT(token || ' ');
-//     console.log('decoded', decoded);
-//     return NextResponse.redirect(new URL(`/users/${decoded._id}`, request.url));
-//   }
-
-//   return NextResponse.next();
-// }
-
-// export const config = {
-//   matcher: ['/((?!_next/static|_next/image|favicon.ico|public).*)'],
-// };
 
 export const config = {
   matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],

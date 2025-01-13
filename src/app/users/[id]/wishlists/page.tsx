0fddef5b38a464ba8bcc535a87af355bd48a22bd
Dima <skydmytro@gmail.com>
2025-01-13 18:31:11 +0100

    feat: add userNotFound component

--- src/app/users/[id]/wishlists/page.tsx
+++ src/app/users/[id]/wishlists/page.tsx
@@ -1,6 +1,7 @@
 import { getRequest } from '@/api/requests';
 import { auth } from '@/auth/authSetup';
 import { WishListPage } from '@/components/WishListsPage/WishListPage';
+import { UserNotFound } from '@/components/WishListsPage/ui/UserNotFound';
 import { UserType } from '@/types/user';
 import { wishListResponse } from '@/types/wishList';
 import { API_URL, wishlistUrl } from '@/utils/config';
@@ -24,13 +25,7 @@ const WishlistsPage = async ({ params }: { params: { id: string } }) => {
     .catch((e) => console.log(e));
 
   if (!user) {
-    return (
-      <div className="min-h-screen bg-gradient-to-b from-gray-900 to-black p-8 text-white ">
-        <div className="flex items-center justify-center">
-          <p className="text-2xl font-semibold">User not found</p>
-        </div>
-      </div>
-    );
+    return <UserNotFound />;
   }
 
   return (

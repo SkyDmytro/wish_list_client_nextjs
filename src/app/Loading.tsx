495e32c7eae56fc261c2d4fb84cc5e35df65e528
Dima <skydmytro@gmail.com>
2025-01-18 18:46:31 +0100

    feat: edit wishlists table

--- src/app/Loading.tsx
+++ src/app/Loading.tsx
@@ -1,3 +1,7 @@
 export const Loading = () => {
-  return <div>Loading....</div>;
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-gray-900 to-black p-8">
+      <div className="h-5 w-5 border-b-2 border-white rounded-full animate-spin"></div>
+    </div>
+  );
 };

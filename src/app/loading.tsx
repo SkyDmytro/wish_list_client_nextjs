8ff3bb20154847f98ad01a4c9087bec5d5c88e6e
Dima <skydmytro@gmail.com>
2025-01-18 19:11:50 +0100

    feat: add loading

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,11 @@
+import { Spinner } from '@/components/Loading/Loading';
+
 export const Loading = () => {
+  console.log('loading');
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-gray-900 to-black p-8">
-      <div className="h-5 w-5 border-b-2 border-white rounded-full animate-spin"></div>
+      <Spinner />
     </div>
   );
 };
+export default Loading;

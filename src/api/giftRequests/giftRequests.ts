2ab8538d0131392072ecfc359796e700acd3a5e0
Dima <skydmytro@gmail.com>
2025-01-30 17:33:41 +0100

    feat: add request to update gift

--- src/api/giftRequests/giftRequests.ts
+++ src/api/giftRequests/giftRequests.ts
@@ -2,7 +2,7 @@ import { createGiftRequestBodyType } from '@/types/requests';
 import { GiftResponse } from '@/types/wishList';
 import { wishlistUrl } from '@/utils/config';
 
-import { deleteRequest, postRequest } from '../requests';
+import { deleteRequest, postRequest, putRequest } from '../requests';
 
 export const createGiftRequest = async (
   gift: createGiftRequestBodyType,
@@ -19,4 +19,16 @@ export const createGiftRequest = async (
 
 export const deleteGiftRequest = async (id: string, token?: string) => {
   return deleteRequest(`${wishlistUrl}/${id}/items`, token);
+};
+
+export const updateGiftRequest = async (
+  id: string,
+  gift: createGiftRequestBodyType,
+  token?: string,
+) => {
+  return putRequest<createGiftRequestBodyType, GiftResponse>(
+    `${wishlistUrl}/${id}/items`,
+    gift,
+    token,
+  );
 };

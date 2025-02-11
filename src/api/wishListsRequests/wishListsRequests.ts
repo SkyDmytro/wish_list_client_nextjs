f1d41021f97ccdfb85df61be2c50827d53bd80fe
Dima <skydmytro@gmail.com>
2025-02-11 16:58:40 +0100

    feat: add wishlist edit request

--- src/api/wishListsRequests/wishListsRequests.ts
+++ src/api/wishListsRequests/wishListsRequests.ts
@@ -1,14 +1,29 @@
-import { createWishListRequestBodyType } from '@/types/requests';
+import {
+  createWishListRequestBodyType,
+  editWishListRequestBodyType,
+} from '@/types/requests';
 import { WishListResponse } from '@/types/wishList';
 import { wishlistUrl } from '@/utils/config';
 
-import { postRequest } from '../requests';
+import { postRequest, putRequest } from '../requests';
 
 export const createWishListRequest = async (
   wishList: createWishListRequestBodyType,
   token?: string,
 ) => {
   return postRequest<createWishListRequestBodyType, WishListResponse>(
+    `${wishlistUrl}/`,
+    wishList,
+    token,
+  );
+};
+
+export const editWishListRequest = async (
+  id: string,
+  wishList: editWishListRequestBodyType,
+  token?: string,
+) => {
+  return putRequest<editWishListRequestBodyType, WishListResponse>(
     `${wishlistUrl}/`,
     wishList,
     token,

ad7a06a7054cc910a913bd82e3e9e5ed623d30d5
Dima <skydmytro@gmail.com>
2025-02-14 10:45:00 +0100

    add Search input to header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -14,6 +14,7 @@ import { signOut, useSession } from 'next-auth/react';
 import Link from 'next/link';
 
 import { Button } from '../ui/button';
+import { Search } from './ui/Search';
 
 export const Header = () => {
   const { data: session } = useSession();
@@ -41,6 +42,7 @@ export const Header = () => {
         </h1>
       </div>
       <div className="flex items-center gap-2">
+        <Search />
         <Button
           variant="ghost"
           size="default"

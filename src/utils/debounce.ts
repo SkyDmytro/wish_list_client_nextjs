d705a09935f85b825cc54a358ea0661345a0b964
Dima <skydmytro@gmail.com>
2025-02-18 15:09:00 +0100

    feat: add debounce function

--- src/utils/debounce.ts
+++ src/utils/debounce.ts
@@ -3,8 +3,8 @@
  * @param {number} delay - The time to wait before calling the function.
  * @returns {Function} A debounced version of the function.
  */
-export const debounced = <T extends unknown[]>(
-  func: (...args: T) => void,
+export const debounced = <T extends unknown[], R>(
+  func: (...args: T) => R,
   delay: number,
 ): ((...args: T) => void) => {
   let timer: NodeJS.Timeout | null = null;

a18594c459882eeb69afc316bc948c3a93f4974f
Dima <skydmytro@gmail.com>
2025-12-22 21:19:49 +0100

    refactor: remove deprecated files

--- src/api/login/loginUserRequest.ts
+++
@@ -1,8 +0,0 @@
-import { LoginSchema } from '@/schemas/userLogin.schema';
-import { userUrl } from '@/utils/config';
-
-import { postRequest } from '../requests';
-
-export const loginUserRequest = async (userLoginData: LoginSchema) => {
-  return postRequest(`${userUrl}/auth`, userLoginData);
-};
